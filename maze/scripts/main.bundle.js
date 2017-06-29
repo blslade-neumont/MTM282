325e11047217f9cf3385deb734e9f86681cbb5c8
aboveyou00 <brandonyoyoslade@gmail.com>
2017-06-29 08:11:49 -0600

    feat(maze): render starting square

--- maze/scripts/main.bundle.js
+++ maze/scripts/main.bundle.js
@@ -20,7 +20,10 @@
     }
     
     function initResize() {
-        body.onresize = function (e) { return refreshCanvasSize(); };
+        body.onresize = function (e) {
+            refreshCanvasSize();
+            drawMaze();
+        };
     }
     function refreshCanvasSize() {
         if (canvas) {
@@ -32,7 +35,7 @@
     var CELL_SIZE = [16, 16];
     var MAZE_SIZE = [1, 1];
     
-    var maze = [];
+    var maze = null;
     
     function generateMaze() {
         MAZE_SIZE = [Math.floor(canvas.width / CELL_SIZE[0]), Math.floor(canvas.height / CELL_SIZE[1])];
@@ -40,18 +43,31 @@
         for (let x = 0; x < MAZE_SIZE[0]; x++) {
             let col = maze[x] = [];
             for (let y = 0; y < MAZE_SIZE[1]; y++) {
-                col[y] = Math.floor(Math.random() * 2);
+                col[y] = -1;
             }
         }
+        
+        let side = Math.floor(Math.random() * 4);
+        let x, y;
+        if (side === 0 || side === 2) {
+            x = (side === 0) ? 1 : MAZE_SIZE[0] - 2;
+            y = Math.floor(Math.random() * (MAZE_SIZE[1] - 1) / 2) * 2 + 1;
+        }
+        else {
+            x = Math.floor(Math.random() * (MAZE_SIZE[0] - 1) / 2) * 2 + 1;
+            y = (side === 1) ? 1 : MAZE_SIZE[1] - 2;
+        }
+        maze[x][y] = 0;
     }
     function drawMaze() {
         context.fillStyle = 'black';
         context.fillRect(0, 0, CELL_SIZE[0], CELL_SIZE[1]);
+        if (!maze) return;
         for (let x = 0; x < MAZE_SIZE[0]; x++) {
             let col = maze[x];
             for (let y = 0; y < MAZE_SIZE[1]; y++) {
                 let cell = col[y];
-                context.fillStyle = cell == 0 ? 'black' : 'red';
+                context.fillStyle = cell == -1 ? 'black' : 'red';
                 context.fillRect(x * CELL_SIZE[0], y * CELL_SIZE[1], CELL_SIZE[0], CELL_SIZE[1]);
             }
         }
