9f0127581eb9a9ab5301ae4e844aee7d88cec053
aboveyou00 <brandonyoyoslade@gmail.com>
2017-07-09 20:50:03 -0600

    feat(maze): add player and enemy; add timer; add fireworks to the victory screen

--- maze/scripts/main.bundle.js
+++ maze/scripts/main.bundle.js
@@ -6,7 +6,7 @@
     let context = null;
     let canvasSize = [500, 500];
     
-    let CELL_SIZE = [48, 48];
+    let CELL_SIZE = [80, 80];
     
     let maze = null;
     
@@ -66,8 +66,9 @@
         this.width = width;
         this.height = height;
         this.maze = null;
-        this.realPlayer = { enabled: false };
+        this.realPlayer = { enabled: true, victory: false, defeat: false };
         this.aiPlayer = { enabled: false };
+        this.seconds = 10;
     }
     Maze.prototype.generate = function() {
         this.maze = [];
@@ -141,6 +142,23 @@
             }
         }
         this.startPos = [x, y];
+        
+        if (this.realPlayer.enabled) {
+            this.realPlayer.x = this.startPos[0];
+            this.realPlayer.y = this.startPos[1];
+            this.realPlayer.hspeed = 0;
+            this.realPlayer.vspeed = 0;
+        }
+        if (this.aiPlayer.enabled) {
+            this.aiPlayer.x = this.startPos[0];
+            this.aiPlayer.y = this.startPos[1];
+            this.aiPlayer.hspeed = 0;
+            this.aiPlayer.vspeed = 0;
+            this.aiPlayer.frontx = 0;
+            this.aiPlayer.fronty = 0;
+        }
+        
+        this.seconds = (Math.floor(this.width / 2) * Math.floor(this.height / 2));
     };
     Maze.prototype.isInMaze = function(x, y) {
         return x >= 0 && y >= 0 && x < this.width && y < this.height;
@@ -158,6 +176,10 @@
                 if (cell !== -1) {
                     context.fillStyle = 'white';
                     context.fillRect(x - .1, y - .1, 1.2, 1.2);
+                }
+                if (cell === 0) {
+                    context.fillStyle = 'rgb(220, 220, 220)';
+                    context.fillRect(x + .2, y + .2, .6, .6);
                 }
             }
         }
@@ -191,6 +213,48 @@
         context.textAlign = 'left';
         context.textBaseline = 'middle';
         context.fillText('Press P to play, press A to watch the AI play', CELL_SIZE[0], CELL_SIZE[1] / 2);
+        
+        context.fillStyle = 'white';
+        context.font = '16px Cambria';
+        context.textAlign = 'center';
+        context.textBaseline = 'middle';
+        let seconds = this.seconds;
+        context.fillText(`Time left: ${Math.floor(seconds)}.${Math.floor((seconds - Math.floor(seconds)) * 10)} seconds`, canvas.width / 2, CELL_SIZE[1] / 2);
+        
+        if (this.realPlayer.enabled && (this.realPlayer.victory || this.realPlayer.defeat)) {
+            context.fillStyle = 'rgba(0, 0, 0, .9)';
+            context.fillRect(0, 0, canvas.width, canvas.height);
+            
+            context.fillStyle = 'white';
+            context.textAlign = 'center';
+            context.textBaseline = 'middle';
+            
+            if (this.realPlayer.victory) {
+                context.font = '42px Cambria';
+                context.fillText(`You Win!`, canvas.width / 2, (canvas.height / 2) - 40);
+                
+                context.font = '16px Cambria';
+                let seconds = this.seconds;
+                context.fillText(`You reached the exit before time ran out.`, canvas.width / 2, (canvas.height / 2) + 20);
+                context.fillText(`Time Remaining: ${Math.floor(seconds)}.${Math.floor((seconds - Math.floor(seconds)) * 10)} seconds`, canvas.width / 2, (canvas.height / 2) + 40);
+                context.fillText(`Press F5 to restart.`, canvas.width / 2, (canvas.height / 2) + 60);
+                
+                if (!this.fireworks) this.fireworks = [];
+                for (let q = 0; q < this.fireworks.length; q++) {
+                    let firework = this.fireworks[q];
+                    firework.render(context);
+                }
+            }
+            else {
+                context.font = '42px Cambria';
+                context.fillText(`You Lose!`, canvas.width / 2, (canvas.height / 2) - 40);
+                
+                context.font = '16px Cambria';
+                let seconds = this.seconds;
+                context.fillText(`Time ran out before you could complete the maze.`, canvas.width / 2, (canvas.height / 2) + 20);
+                context.fillText(`If the maze is too large, reduce the size of your browser tab and press F5 to try again.`, canvas.width / 2, (canvas.height / 2) + 40);
+            }
+        }
     };
     Maze.prototype.events = function() {
         this.keys = new Map();
@@ -229,22 +293,52 @@
     }
     Maze.prototype.tick = function() {
         if (this.realPlayer.enabled) {
-            this.realPlayer.x += this.realPlayer.hspeed;
-            this.realPlayer.y += this.realPlayer.vspeed;
-            let roundX = Math.round(this.realPlayer.x);
-            let roundY = Math.round(this.realPlayer.y);
-            if (Math.abs(this.realPlayer.x - roundX) < .01 && Math.abs(this.realPlayer.y - roundY) < .01) {
-                this.realPlayer.x = roundX;
-                this.realPlayer.y = roundY;
-                this.realPlayer.hspeed = 0;
-                this.realPlayer.vspeed = 0;
-                let mvx = 0 + ((this.keys.has('ArrowRight') && this.keys.get('ArrowRight')) ? 1 : 0) - ((this.keys.has('ArrowLeft') && this.keys.get('ArrowLeft')) ? 1 : 0);
-                let mvy = 0 + ((this.keys.has('ArrowDown') && this.keys.get('ArrowDown')) ? 1 : 0) - ((this.keys.has('ArrowUp') && this.keys.get('ArrowUp')) ? 1 : 0);
-                if (mvx !== 0 && this.isInMaze(this.realPlayer.x + mvx, this.realPlayer.y) && this.maze[this.realPlayer.x + mvx][this.realPlayer.y] !== -1) {
-                    this.realPlayer.hspeed = mvx / 8;
+            if (!this.realPlayer.victory && !this.realPlayer.defeat) {
+                this.realPlayer.x += this.realPlayer.hspeed;
+                this.realPlayer.y += this.realPlayer.vspeed;
+                let roundX = Math.round(this.realPlayer.x);
+                let roundY = Math.round(this.realPlayer.y);
+                if (Math.abs(this.realPlayer.x - roundX) < .01 && Math.abs(this.realPlayer.y - roundY) < .01) {
+                    this.realPlayer.x = roundX;
+                    this.realPlayer.y = roundY;
+                    this.realPlayer.hspeed = 0;
+                    this.realPlayer.vspeed = 0;
+                    if (roundX === this.endPos[0] && roundY === this.endPos[1]) {
+                        this.realPlayer.victory = true;
+                    }
+                    else {
+                        let mvx = 0 + ((this.keys.has('ArrowRight') && this.keys.get('ArrowRight')) ? 1 : 0) - ((this.keys.has('ArrowLeft') && this.keys.get('ArrowLeft')) ? 1 : 0);
+                        let mvy = 0 + ((this.keys.has('ArrowDown') && this.keys.get('ArrowDown')) ? 1 : 0) - ((this.keys.has('ArrowUp') && this.keys.get('ArrowUp')) ? 1 : 0);
+                        if (mvx !== 0 && this.isInMaze(this.realPlayer.x + mvx, this.realPlayer.y) && this.maze[this.realPlayer.x + mvx][this.realPlayer.y] !== -1) {
+                            this.realPlayer.hspeed = mvx / 8;
+                        }
+                        else if (mvy !== 0 && this.isInMaze(this.realPlayer.x, this.realPlayer.y + mvy) && this.maze[this.realPlayer.x][this.realPlayer.y + mvy] !== -1) {
+                            this.realPlayer.vspeed = mvy / 8;
+                        }
+                    }
                 }
-                else if (mvy !== 0 && this.isInMaze(this.realPlayer.x, this.realPlayer.y + mvy) && this.maze[this.realPlayer.x][this.realPlayer.y + mvy] !== -1) {
-                    this.realPlayer.vspeed = mvy / 8;
+            }
+            
+            if (this.seconds <= 0 && !this.realPlayer.victory) this.realPlayer.defeat = true;
+            
+            if (this.realPlayer.defeat) {
+                
+            }
+            if (this.realPlayer.victory) {
+                //TODO: something cool
+                if (!this.fireworks) this.fireworks = [];
+                if (!this.fireworksTimer || this.fireworksTimer <= 0) {
+                    this.fireworksTimer = .25;
+                    this.fireworks.push(this.createFirework());
+                }
+                else this.fireworksTimer -= 1 / 30;
+                for (let q = 0; q < this.fireworks.length; q++) {
+                    let firework = this.fireworks[q];
+                    if (!firework.tick()) {
+                        this.fireworks.splice(q, 1);
+                        q--;
+                        continue;
+                    }
                 }
             }
         }
@@ -278,9 +372,60 @@
     };
     Maze.prototype.loop = function() {
         setInterval(() => {
+            if (!this.realPlayer.victory && !this.realPlayer.defeat) this.seconds -= 1 / 30;
             this.tick();
             this.render(context);
         }, 1000 / 30);
+    };
+    
+    Maze.prototype.createFirework = function() {
+        let obj = {
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            color: ['green', 'red', 'yellow', 'blue', 'purple'][Math.floor(Math.random() * 5)],
+            parts: []
+        };
+        for (let q = 0; q < 100; q++) {
+            let dir = Math.random() * 2 * Math.PI;
+            let spd = Math.random() * 5;
+            obj.parts[q] = {
+                x: 0,
+                y: 0,
+                hspeed: Math.cos(dir) * spd,
+                vspeed: Math.sin(dir) * spd,
+                ttl: 1 + Math.random()
+            }
+        }
+        
+        obj.tick = (function() {
+            isAlive = false;
+            for (let q = 0; q < this.parts.length; q++) {
+                let part = this.parts[q];
+                if (part.ttl > 0) isAlive = true;
+                part.ttl -= 1 / 30;
+                part.x += part.hspeed;
+                part.y += part.vspeed;
+            }
+            return isAlive;
+        }).bind(obj);
+        
+        obj.render = (function(context) {
+            context.save();
+            context.translate(this.x, this.y);
+            context.fillStyle = this.color;
+            
+            for (let q = 0; q < this.parts.length; q++) {
+                let part = this.parts[q];
+                if (part.ttl > 0) {
+                    context.globalAlpha = Math.min(1, part.ttl);
+                    context.fillRect(part.x - 3, part.y - 3, 6, 6);
+                }
+            }
+            
+            context.restore();
+        }).bind(obj);
+        
+        return obj;
     };
     
     init();
